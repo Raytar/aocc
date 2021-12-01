1966f0775fdb3124920b76cf6a95b72621537564
John Ingve Olsen <johningveolsen@gmail.com>
2021-12-01 14:31:27 +0100

    more vector tests

--- test/aoc/vec_test.c
+++ test/aoc/vec_test.c
@@ -9,7 +9,7 @@ vec_int_t *v;
 
 void setUp()
 {
-	v = vec_int_new();
+	v = vec_int_new_with_capacity(1);
 }
 
 void tearDown()
@@ -40,11 +40,50 @@ void test_vec_insert_back()
 	TEST_ASSERT_EQUAL_INT(val, vec_int_get(v, 1));
 }
 
+void test_vec_remove()
+{
+	vec_int_push_back(v, 1);
+	vec_int_push_back(v, 2);
+	vec_int_push_back(v, 3);
+
+	vec_int_remove(v, 1);
+
+	TEST_ASSERT_EQUAL_INT(3, vec_int_pop_back(v));
+	TEST_ASSERT_EQUAL_INT(1, vec_int_pop_back(v));
+	TEST_ASSERT_EQUAL_INT(0, vec_int_size(v));
+}
+
+void test_vec_clear()
+{
+	vec_int_push_back(v, 1);
+	vec_int_push_back(v, 2);
+	vec_int_push_back(v, 3);
+
+	vec_int_clear(v);
+
+	TEST_ASSERT_EQUAL_INT(0, vec_int_size(v));
+}
+
+void test_vec_grow()
+{
+	vec_int_push_back(v, 1);
+	vec_int_push_back(v, 2);
+	vec_int_push_back(v, 3);
+
+	vec_int_ensure_capacity(v, 100);
+
+	TEST_ASSERT_EQUAL_INT(3, vec_int_pop_back(v));
+	TEST_ASSERT_EQUAL_INT(2, vec_int_pop_back(v));
+	TEST_ASSERT_EQUAL_INT(1, vec_int_pop_back(v));
+}
+
 int main()
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_vec_push_and_pop);
 	RUN_TEST(test_vec_insert_front);
 	RUN_TEST(test_vec_insert_back);
+	RUN_TEST(test_vec_remove);
+	RUN_TEST(test_vec_clear);
 	return UNITY_END();
 }
