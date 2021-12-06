511125fba0a152c371b30af7ad8030c91b848afe
John Ingve Olsen <johningveolsen@gmail.com>
2021-12-06 22:54:50 +0100

    day6 refactor

--- src/2021/day6.c
+++ src/2021/day6.c
@@ -27,47 +27,24 @@ int64_t day6(FILE *input, int days)
 		token = strtok(NULL, ",");
 	}
 
-	vec_uint8_sort(starting_fishes, &cmp_uint8);
 	queue_int64_t *queue = queue_int64_new(9);
-
-	uint8_t days_until_spawn = 0;
-	uint64_t num_fishes = 0;
-	for (size_t i = 0; i < vec_uint8_size(starting_fishes); i++)
-	{
-		num = vec_uint8_get(starting_fishes, i);
-
-		while (num > days_until_spawn)
-		{
-			days_until_spawn++;
-			queue_int64_enqueue(queue, num_fishes);
-			num_fishes = 0;
-		}
-
-		if (num < days_until_spawn)
-			panic("expected the vector to be sorted");
-
-		num_fishes++;
-	}
-	queue_int64_enqueue(queue, num_fishes);
 
 	while (queue_int64_len(queue) < 9)
 		queue_int64_enqueue(queue, 0);
 
+	for (size_t i = 0; i < vec_uint8_size(starting_fishes); i++)
+	{
+		uint8_t days = vec_uint8_get(starting_fishes, i);
+		int64_t num_fishes = queue_int64_get(queue, days);
+		queue_int64_set(queue, days, ++num_fishes);
+	}
+
 	for (int i = 0; i < days; i++)
 	{
-		num_fishes = queue_int64_dequeue(queue);
-		uint64_t n = queue_int64_get(queue, 6);
+		int64_t num_fishes = queue_int64_dequeue(queue);
+		int64_t n = queue_int64_get(queue, 6);
 		queue_int64_set(queue, 6, n + num_fishes); // increase the number of fishes with 7 days left
 		queue_int64_enqueue(queue, num_fishes);	   // the new fishes with 8 days left
-
-		int64_t total = 0;
-		for (size_t i = 0; i < queue_int64_len(queue); i++)
-		{
-			uint64_t m = queue_int64_get(queue, i);
-			total += m;
-		}
-
-		printf("day %d: %" PRIu64 "\n", i + 1, total);
 	}
 
 	int64_t total = 0;
